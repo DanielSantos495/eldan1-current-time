312350f4e36e8550a824f77649e42c2c1e72b2c9
Daniel Santos <dsantos0495@gmail.com>
2020-09-12 14:57:11 -0500

    probando en como paquete antes de subirlo

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const dateCurrent = new Date();
       twelveHoursNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
       twelveHoursName = ['Una','Dos','Tres','Cuatro','Cinco','Seis','Siete','Ocho','Nueve','Diez','Once','Doce'];
 
-let hours = dateCurrent.getHours() < 10 ? `${0}${dateCurrent.getHours()}` : dateCurrent.getHours();
+let hours = dateCurrent.getHours();
     minutes = dateCurrent.getMinutes() < 10 ? `${0}${dateCurrent.getMinutes()}`: dateCurrent.getMinutes();
     seconds = dateCurrent.getSeconds() < 10 ? `${0}${dateCurrent.getSeconds()}` : dateCurrent.getSeconds();
 
-const time = (ampm) => `${hours}:${minutes}:${seconds} ${ampm}`;
+const time = (hour, ampm) => {
+
+  let hourFixed = hour < 10 ? `${0}${hour}` : hour;
+  return `${hourFixed}:${minutes}:${seconds} ${ampm}`;
+
+};
 
 const currentHour = () => {
 
@@ -14,13 +19,14 @@ const currentHour = () => {
   if (hours >= twelveHoursNumber[11]) {
 
     hours = hours === 12 ? hours : hours - 12;
+    // hours = hours < 10 ? `${0}${hours}` : hours;
 
     if ( hours === twelveHoursNumber[11] ) {
       // Tanda midday
-      console.log(`Hora actual: ${time('PM')}\n${twelveHoursName[11]} del medio día.`);
+      console.log(`Hora actual: ${time(hours, 'PM')}\n${twelveHoursName[11]} del medio día.`);
     } else if ( hours >= twelveHoursNumber[0] && hours < twelveHoursNumber[6] ) {
       // Tanda afternoon
-      console.log(`Hora actual: ${time('PM')}`);
+      console.log(`Hora actual: ${time(hours, 'PM')}`);
       if(hours === twelveHoursNumber[0]) console.log(`${twelveHoursName[0]} de la tarde.`);
       if(hours === twelveHoursNumber[1]) console.log(`${twelveHoursName[1]} de la tarde.`);
       if(hours === twelveHoursNumber[2]) console.log(`${twelveHoursName[2]} de la tarde.`);
@@ -30,7 +36,7 @@ const currentHour = () => {
 
     } else if ( hours >= twelveHoursNumber[6] && hours < twelveHoursNumber[11] ) {
       // Tanda night
-      console.log(`Hora actual: ${time('PM')}`);
+      console.log(`Hora actual: ${time(hours,'PM')}`);
       if(hours === twelveHoursNumber[6]) console.log(`${twelveHoursName[6]} de la noche.`);
       if(hours === twelveHoursNumber[7]) console.log(`${twelveHoursName[7]} de la noche.`);
       if(hours === twelveHoursNumber[8]) console.log(`${twelveHoursName[8]} de la noche.`);
@@ -43,14 +49,15 @@ const currentHour = () => {
   // After midnight
 
     hours = hours === 0 ? 12 : hours;
+    // hours = hours < 10 ? `${0}${hours}` : hours;
 
     if ( hours === twelveHoursNumber[11] ) {
       //For requirements this hour is validate as nigth, not early morning. Tanda midnight
-      console.log(`Hora actual: ${time('PM')}\n${twelveHoursName[11]} de la noche.`);
+      console.log(`Hora actual: ${time(hours,'PM')}\n${twelveHoursName[11]} de la noche.`);
     }
     if ( hours >= twelveHoursNumber[0] && hours < twelveHoursNumber[5] ) {
       // Tanda early mornig
-      console.log(`Hora actual: ${time('AM')}`);
+      console.log(`Hora actual: ${time(hours,'AM')}`);
       if(hours === twelveHoursNumber[0]) console.log(`${twelveHoursName[0]} de la madrugada.`);
       if(hours === twelveHoursNumber[1]) console.log(`${twelveHoursName[1]} de la madrugada.`);
       if(hours === twelveHoursNumber[2]) console.log(`${twelveHoursName[2]} de la madrugada.`);
@@ -59,7 +66,7 @@ const currentHour = () => {
 
     } else if ( hours >= twelveHoursNumber[5] && hours < twelveHoursNumber[11] ) {
       // Tanda morning
-      console.log(`Hora actual: ${time('AM')}`);
+      console.log(`Hora actual: ${time(hours,'AM')}`);
       if(hours === twelveHoursNumber[5]) console.log(`${twelveHoursName[5]} de la mañana.`);
       if(hours === twelveHoursNumber[6]) console.log(`${twelveHoursName[6]} de la mañana.`);
       if(hours === twelveHoursNumber[7]) console.log(`${twelveHoursName[7]} de la mañana.`);
@@ -72,4 +79,4 @@ const currentHour = () => {
   }
 }
 
-module.exports = { currentHour };
+module.exports = { currentHour }
